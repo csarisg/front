4d0eeaf1cf52ab884f7c67e04fa367cd6447ef71
sara <csaragonzalez>
2025-05-25 14:41:51 -0500

    ahora si

--- ejclase8/ejersicio3.js
+++ ejclase8/ejersicio3.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")({ sigint: true })
 
-let a = parseFloat(prompt("ingrese dos numeros: "))
+let a = parseFloat(prompt("ingrese un numeros: "))
 let b = parseFloat(prompt("ingrese otro numero: "))
 
 const resultado = a + b
