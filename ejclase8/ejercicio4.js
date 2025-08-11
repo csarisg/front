c751752018f84ee95de78d749af81b88c7ad353f
Sara Cantillo <csaragonzalez@gmail.com>
2025-08-11 15:29:20 -0500

    Delete ejclase8 directory

--- ejclase8/ejercicio4.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true })
-
-let numero = prompt("ingrese un numero: ")
-const resultado = numero % 2
-
-if (resultado === 0){console.log("numero par")}
-else {console.log("numero impar")}
