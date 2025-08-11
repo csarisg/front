0191a6756e00affd19e34f1b8ed6f5327eeba551
Sara Cantillo <csaragonzalez@gmail.com>
2025-08-11 15:31:53 -0500

    Delete clase9 directory

--- clase9/ejercicio1.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true });
-
-let a = parseFloat(prompt("ingrese un numeros: "))
-let b = parseFloat(prompt("ingrese otro numero: "))
-
-if (b > a){console.log(b +" es mayor que "+ a)}
-else if (a > b){console.log(a +" es mayos que "+ b)}
-else if(a = b){console.log(a +" es igual a "+ b)}
-
