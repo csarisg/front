5d2a112c2f8a313a298627b1d43ab4542e700cbd
Sara Cantillo <csaragonzalez@gmail.com>
2025-08-11 15:31:42 -0500

    Delete clase8 directory

--- clase8/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true });
-
-
-let numero = prompt("ingrese un numero: ");
-const resultado = numero * 2
-
-console.log("resultado:", resultado);
-
--- clase8/ejercicio2.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true })
-
-let numero = parseFloat(prompt("ingrese numero: "))
-
-
-if (numero > 0) {console.log("numero positivo")}
-else if (numero < 0) {console.log("numero negativo")}
-else {console.log("0")}
-
